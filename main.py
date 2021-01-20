9f9f639c0939dfee03d1d1c05c6f44dc07d20fa1
Nahid <nahidfarzad8751@gmail.com>
2021-01-20 08:39:53 -0500

    adding flask

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template 
+
+app = Flask(main.py)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+if main.py == "__main__":
+    app.run(debug=True
